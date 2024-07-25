6238031f48146b3c53d59f771033f689751b631a
Abdulrahmanololade <abdulrahmanololade15@gmail.com>
2024-07-25 14:22:59 +0100

    firt commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function toggleDiv(){
+    if(document.getElementById("toggleDiv").style.display="block"){
+        document.getElementById("toggleDiv").style.display="none";
+    }else{
+        document.getElementById("toggleDiv").style.display="block";
+    }
+}
